0472469231b0ab5a3dbe4bd3202fb4d5e22622c1
Azad <poursdeg@gmail.com>
2019-02-02 17:37:15 +0330

    lalala

--- mysite/main/models.py
+++ mysite/main/models.py
@@ -1,3 +1,9 @@
-from django.db import models
-
-# Create your models here.
+from django.urls import reverse
+class Post(models.Model):
+# ...
+def get_absolute_url(self):
+return reverse('blog:post_detail',
+args=[self.publish.year,
+self.publish.month,
+self.publish.day,
+self.slug])
