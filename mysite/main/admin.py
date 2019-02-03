73fbad31580c0a2be236e0e8a628525d4b61c6fc
Azad <poursdeg@gmail.com>
2019-02-03 15:23:10 +0330

    every thing is running fine and i skiped tinymce in windows machine but working in linux

--- mysite/main/admin.py
+++ mysite/main/admin.py
@@ -1,16 +1,12 @@
 from django.contrib import admin
 from .models import Tutorial
 
+
 class TutorialAdmin(admin.ModelAdmin):
-	#fields = ['Tutorial_title',
-	#		  'Tutorial_published',
-	#		  'Tutorial_content',
-	#			]
-	fieldset = [
-			("Title/date", {'fields': ["Tutorial_title", "Tutorial_published"]}),
-			("Content", { 'fields': [ "Tutorial_content"]})
 
-			]
+    fieldsets = [
+        ("Title / Date", {'fields': ["Tutorial_title", "Tutorial_published"]}),
+        ("Content", {"fields": ["Tutorial_content"]})
+    ]
 
-admin.site.register(Tutorial, TutorialAdmin)
-
+admin.site.register(Tutorial,TutorialAdmin)
--- mysite/main/models.py
+++ mysite/main/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-import datetime
+from datetime import datetime
 
 class Tutorial(models.Model):
 	Tutorial_title = models.CharField(max_length=200)
 	Tutorial_content = models.TextField()
 	Tutorial_published = models.DateTimeField("date published")
-	Tutorial_edit = models.DateTimeField("Date Edited", default=datetime.datetime.now )
+	Tutorial_edit = models.DateTimeField("Date Edited", default=datetime.now )
 
 
 	def __str__(self):
