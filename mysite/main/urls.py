348f36ed2ea5b3aed16617d74731151afbf95a98
Azad <poursdeg@gmail.com>
2019-02-04 17:42:24 +0330

    css added

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = "main"
+
 urlpatterns = [
 			path("", views.homepage, name="homepage"),
+			path("register/", views.register, name="register")
 			
 ]
