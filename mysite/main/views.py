fa9304696976387d45ec24669f9e9c49aa28f97a
Azad <poursdeg@gmail.com>
2019-02-10 14:51:37 +0330

    new changes

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from .models import Tutorial
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import logout, authenticate, login
-from django.contrib import messages
+from django.contrib import messages 
 
 
 # Create your views here.
@@ -20,8 +20,10 @@ def register(request):
             username = form.cleaned_data.get('username')
             messages.success(request, f"New account created: {username}")
             login(request, user)
+            messages.info(request, f"You are logged in as : {username}")
+            login(request, user)
             return redirect("main:homepage")
-
+ 
         else:
             for msg in form.error_messages:
                 messages.error(request, f'{msg}: form.error_messages[msg])')
