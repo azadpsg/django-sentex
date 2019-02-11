bdf0ab9abb61f39323af1823fbcc34a1b7082091
azadpsg <poursadeg@gmail.com>
2019-02-11 19:32:27 +0330

    new models created

--- mysite/main/admin.py
+++ mysite/main/admin.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
-from .models import Tutorial
+from .models import Tutorial, TutorialSeries, TutorialCategory
 
 
 class TutorialAdmin(admin.ModelAdmin):
 
     fieldsets = [
         ("Title / Date", {'fields': ["Tutorial_title", "Tutorial_published"]}),
-        ("Content", {"fields": ["Tutorial_content"]})
+        ("Content", {"fields": ["Tutorial_content"]}),
+        ("Series", {'fields': ["Tutorial_series"]}),
+        ("URL", {'fields': ["Tutorial_slug"]}),
     ]
 
+
 admin.site.register(Tutorial,TutorialAdmin)
+admin.site.register(TutorialSeries)
+admin.site.register(TutorialCategory)
--- mysite/main/models.py
+++ mysite/main/models.py
@@ -1,12 +1,36 @@
 from django.db import models
 from datetime import datetime
 
+class TutorialCategory(models.Model):
+	category = models.CharField(max_length=200)
+	summery = models.CharField(max_length=200)
+	slug = models.CharField(max_length=200, default=1)
+	class Meta:
+		#name for admin page
+		verbose_name_plural = "Categories"
+
+	def __str__(self):
+		return self.category
+
+class TutorialSeries(models.Model):
+	tutorial_series = models.CharField(max_length=200)
+
+	category = models.ForeignKey(TutorialCategory, default=1, verbose_name="Category", on_delete=models.SET_DEFAULT)
+	series_summary = models.CharField(max_length=200)
+
+	class Meta:
+		# otherwise we get "Tutorial Seriess in admin"
+		verbose_name_plural = "Series"
+
+	def __str__(self):
+		return self.tutorial_series
+
 class Tutorial(models.Model):
 	Tutorial_title = models.CharField(max_length=200)
 	Tutorial_content = models.TextField()
 	Tutorial_published = models.DateTimeField("date published")
 	Tutorial_edit = models.DateTimeField("Date Edited", default=datetime.now )
-
-
+	Tutorial_series = models.ForeignKey(TutorialSeries, default=1, verbose_name="Series", on_delete=models.SET_DEFAULT)
+	Tutorial_slug = models.CharField(max_length=200, default=1)
 	def __str__(self):
 		return self.Tutorial_title
